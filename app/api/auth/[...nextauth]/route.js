b798abce5797af90f6c51c1b9805f6c01f261826
Rishabh Jain <91656132+RishabhJ-26@users.noreply.github.com>
2025-05-21 21:40:02 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,70 +1,46 @@
-import NextAuth from 'next-auth'
-// import AppleProvider from 'next-auth/providers/apple'
-// import FacebookProvider from 'next-auth/providers/facebook'
-// import GoogleProvider from 'next-auth/providers/google'
-// import EmailProvider from 'next-auth/providers/email'
-import GitHubProvider from "next-auth/providers/github";
-import mongoose from "mongoose";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import connectDb from '@/db/connectDb';
-import User from '@/models/User';
-import Payment from '@/models/Payment';
+import GitHubProvider from "next-auth/providers/github";
+import connectDb from "@/db/connectDb";
+import User from "@/models/User";
 
-export const authoptions = NextAuth({
+const authOptions = {
   providers: [
-    // OAuth authentication providers...
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientSecret: process.env.GITHUB_SECRET,
     }),
-    //   AppleProvider({
-    //     clientId: process.env.APPLE_ID,
-    //     clientSecret: process.env.APPLE_SECRET
-    //   }),
-    //   FacebookProvider({
-    //     clientId: process.env.FACEBOOK_ID,
-    //     clientSecret: process.env.FACEBOOK_SECRET
-    //   }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
-    //   // Passwordless / email sign in
-    //   EmailProvider({
-    //     server: process.env.MAIL_SERVER,
-    //     from: 'NextAuth.js <no-reply@example.com>'
-    //   }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      if (account.provider === "github" || account.provider === "google") {
-        await connectDb();
-        // Check if the user already exists in the database
-        const currentUser = await User.findOne({ email: user.email });
-        if (!currentUser) {
-          // Create a new user
-          await User.create({
-            email: user.email,
-            username: user.email.split("@")[0],
-          });
-        }
-        return true;
+    async signIn({ user, account }) {
+      await connectDb();
+      const existingUser = await User.findOne({ email: user.email });
+      if (!existingUser) {
+        await User.create({
+          email: user.email,
+          username: user.email.split("@")[0],
+        });
       }
-      // Return true by default to not block signIn for other providers
       return true;
     },
-
-    async session({ session, user, token }) {
-      const dbUserDoc = await User.findOne({ email: session.user.email });
-      if (dbUserDoc) {
-        const dbUser = dbUserDoc.toObject();
-        session.user.name = dbUser.username || dbUser.name || session.user.name;
-        session.user.profilepic = dbUser.profilepic || null;
+    async session({ session }) {
+      const dbUser = await User.findOne({ email: session.user.email });
+      if (dbUser) {
+        const userObj = dbUser.toObject();
+        session.user.name = userObj.username || userObj.name || session.user.name;
+        session.user.profilepic = userObj.profilepic || null;
       }
-      // session.user.name = dbUser.username
       return session;
     },
   },
-});
+};
 
-export { authoptions as GET, authoptions as POST };
+const handler = NextAuth(authOptions);
+
+export const GET = handler;
+export const POST = handler;
